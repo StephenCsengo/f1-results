8fd20000b7030f50d3467b8f92f3ebff3f0f0c9d
Stephen Csengo <stephencsengo@gmail.com>
2023-05-10 15:02:55 -0400

    Correctly accessing year info from Ergast API

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    addYear();
+});
+
+/*The addYear function adds the years of available data to
+the select a year portion of the form*/
+function addYear() {
     const selector = document.getElementById("year");
 
     for (let year = 2023; year > 1949; year--) {
@@ -7,4 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
         option.innerHTML= year;
         selector.appendChild(option);
     }
-});
+}
+
+function addRounds(year) {
+    fetch(`https://ergast.com/api/f1/${year}.json`)
+    .then(resp => resp.json())
+    .then(rounds =>console.log(rounds))
+}
+
