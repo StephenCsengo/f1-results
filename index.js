ea6a097ce8e1e46ca4324234c355efef0bea8bf0
Stephen Csengo <stephencsengo@gmail.com>
2023-05-10 20:43:47 -0400

    Formatting of results table

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     addYear();
-    //getRaces(2023);
-    
 });
 
 const yearSelector = document.getElementById("year");
@@ -9,20 +7,29 @@ const raceSelector = document.getElementById("race");
 
 document.getElementById("year-race-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    let resultsSection = document.getElementById("results");
+    let restSection = document.getElementById("rest-of-field");
+    let podiumSection = document.getElementById("podium");
     let yearChoice = yearSelector.value;
     let raceChoice = raceSelector.value;
-    resultsSection.innerHTML = "";
+    restSection.innerHTML = "";
+
     fetch(`https://ergast.com/api/f1/${yearChoice}/${raceChoice}/results.json`)
     .then(resp => resp.json())
     .then(results => {
-        let raceResult = results.MRData.RaceTable.Races[0].Results;
-        
-        raceResult.forEach(function(driver) {
+        const raceResult = results.MRData.RaceTable.Races[0].Results;
+        const topThree = raceResult.slice(0,3);
+        const restOfField = raceResult.slice(3);
+        createFieldResults(restOfField);
+        /*topThree.forEach(function(driver) {
             let result = document.createElement("div");
             result.innerHTML = `${driver.Driver.givenName} ${driver.Driver.familyName} | ${driver.Constructor.name}` ;
-            resultsSection.appendChild(result);
+            podiumSection.appendChild(result);
         })
+        restOfField.forEach(function(driver) {
+            let result = document.createElement("div");
+            result.innerHTML = `${driver.Driver.givenName} ${driver.Driver.familyName} | ${driver.Constructor.name}` ;
+            restSection.appendChild(result);
+        })*/
     })
 });
 /*The addYear function adds the years of available data to
@@ -50,20 +57,30 @@ function getRaces(year) {
         raceSelector.disabled = false;
         raceSelector.innerHTML = "";
         addRaces(races);
-    })
+    });
 }
 
 /*Add races to the select a race dropdown based on what
 year is selected */
 function addRaces(races) {
-
     for(let i = 0; i < races.length; i++) {
         const raceOption = document.createElement("option");
         raceOption.value = races[i].round;
         raceOption.innerHTML = races[i].raceName;
         raceSelector.appendChild(raceOption);
     }
-    raceSelector.addEventListener("change", function() {
-        //console.log(this.value);
-    })
+}
+
+function createFieldResults(drivers) {
+    const table = document.getElementById("rest-of-field");
+    
+    for(let i = 0; i < drivers.length; i++) {
+        const row = table.insertRow(-1);
+        const position = row.insertCell(0);
+        const name = row.insertCell(1);
+        const constructor = row.insertCell(2);
+        position.innerHTML = drivers[i].position;
+        name.innerHTML = `${drivers[i].Driver.givenName} ${drivers[i].Driver.familyName}`;
+        constructor.innerHTML = drivers[i].Constructor.name;
+    }
 }
